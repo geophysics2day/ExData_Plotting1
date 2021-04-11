4337d0ca961bac8e8dde6bec25d17ffb1e042048
geophysics2day <k.phillips.alonge@gmail.com>
2021-04-11 17:06:58 -0600

    Changing location for easy viewing

---
+++ plot4.R
@@ -0,0 +1,32 @@
+colnames <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",col.names = colnames)
+DateTime <- paste(data$Date, data$Time, sep = " ")
+DateFixed <- strptime(DateTime,"%d/%m/%Y %H:%M:%S") ##Standardize the date/time format 
+data$Date <- DateFixed ## Add standardized date/time data to data frame
+## Restrict data to dates of interest
+dataSelect <- subset(data, (Date >= "2007-02-01" & Date <="2007-02-03"), select = c("Date","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+##Plot 4
+dev.set(4)
+par(mfcol = c(2,2),mar = c(4, 5, 2, 2))
+##Top Left
+with(dataSelect, plot(Date,Global_active_power, pch = '.', ylab = "Global Active Power (kilowatts)"), type = "n")
+lines(dataSelect$Date, dataSelect$Global_active_power, pch = '.', type = "l", lty = 1, lwd = 1.5, col = "black")
+
+##Bottom Left
+with(dataSelect, plot(Date,Sub_metering_1, pch = '.', ylab = "Energy sub metering"), type = "n")
+lines(dataSelect$Date, dataSelect$Sub_metering_1, pch = '.', type = "l", lty = 1, lwd = 1.5, col = "black")
+lines(dataSelect$Date, dataSelect$Sub_metering_2, pch = '.', type = "l", lty = 1, lwd = 1.5, col = "red")
+lines(dataSelect$Date, dataSelect$Sub_metering_3, pch = '.', type = "l", lty = 1, lwd = 1.5, col = "blue")
+legend("topright", lty = 1, col = c("black","red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+##Top Right
+with(dataSelect, plot(Date,Voltage, pch = '.', ylab = "Voltage", xlab = "datetime"), type = "n")
+lines(dataSelect$Date, dataSelect$Voltage, pch = '.', type = "l", lty = 1, lwd = 1.5, col = "black")
+
+##Bottom Right
+with(dataSelect, plot(Date,Global_reactive_power, pch = '.', ylab = "Global_reactive_power", xlab = "datetime"), type = "n")
+lines(dataSelect$Date, dataSelect$Global_reactive_power, pch = '.', type = "l", lty = 1, lwd = 1.5, col = "black")
+
+dev.copy(png, file = "plot4.png", width = 480, height = 480) ## Copy to PNG
+dev.off() ## Closing the png
